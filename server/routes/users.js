fdebd227215ba574263fecd36e3787bad2c4580b
Damian mutisya <57178774+DamianMutisya@users.noreply.github.com>
2024-10-20 15:42:53 +0300

    update client

--- server/routes/users.js
+++ server/routes/users.js
@@ -216,7 +216,7 @@ router.post('/forgot-password', async (req, res) => {
       subject: 'Password Reset Link',
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
-        http://localhost:3000/reset-password/${resetToken}\n\n
+        ${process.env.CLIENT_URL}/reset-password/${resetToken}\n\n
         If you did not request this, please ignore this email and your password will remain unchanged.\n`
     };
 
