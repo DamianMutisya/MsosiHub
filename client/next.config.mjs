0ac043cd3538e229f5628dae092abbc00cd0e86d
Damian mutisya <57178774+DamianMutisya@users.noreply.github.com>
2024-10-21 05:11:42 +0300

    import statements are using the '@' alias 2

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,3 +1,9 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
@@ -10,6 +16,10 @@ const nextConfig = {
           pathname: '/**',
         },
       ],
+    },
+    webpack: (config) => {
+      config.resolve.alias['@'] = path.resolve(__dirname, 'client');
+      return config;
     },
   }
 
